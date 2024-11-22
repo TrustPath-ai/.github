f6a4f54fb4ad6cfb5d426d2f4cbd68d9b692a475
Matija Osojnik <matija1.osojnik@gmail.com>
2024-11-22 13:56:07 +0000

    Fix auth script 🔧

--- actions/gcp/auth/scripts/set-environment.sh
+++ actions/gcp/auth/scripts/set-environment.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Get inputs from environment variables that the action sets
+EVENT_NAME="${GITHUB_EVENT_NAME}"
+GITHUB_REF="${GITHUB_REF}"
+INPUT_ENVIRONMENT="${INPUT_ENVIRONMENT}"
+
 # Determine environment
-if [[ "${{ github.event_name }}" == "workflow_dispatch" ]]; then
-  ENV="${{ inputs.environment }}"
-elif [[ ${{ github.ref }} == 'refs/heads/main' ]]; then
+if [[ "${EVENT_NAME}" == "workflow_dispatch" ]]; then
+  ENV="${INPUT_ENVIRONMENT}"
+elif [[ "${GITHUB_REF}" == "refs/heads/main" ]]; then
   ENV="prod"
-elif [[ ${{ github.ref }} == 'refs/heads/staging' ]]; then
+elif [[ "${GITHUB_REF}" == "refs/heads/staging" ]]; then
   ENV="staging"
 else
   ENV="dev"
@@ -14,13 +19,13 @@ fi
 # Set project ID based on environment
 case $ENV in
   "prod")
-    PROJECT_ID="${{ vars.PROD_GCP_PROJECT_ID }}"
+    PROJECT_ID="${PROD_GCP_PROJECT_ID}"
     ;;
   "staging")
-    PROJECT_ID="${{ vars.STAGING_GCP_PROJECT_ID }}"
+    PROJECT_ID="${STAGING_GCP_PROJECT_ID}"
     ;;
   *)
-    PROJECT_ID="${{ vars.DEV_GCP_PROJECT_ID }}"
+    PROJECT_ID="${DEV_GCP_PROJECT_ID}"
     ;;
 esac
 
